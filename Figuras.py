59f3fc68a2425f50a9850bead800caa1ca32a1c3
NABA-OL <NABA-OL@users.noreply.github.com>
2019-09-16 22:28:49 -0500

    Update Figuras.py

--- Figuras.py
+++ Figuras.py
@@ -1,17 +1,31 @@
 from tkinter import *
+import math
+
 
 canvas_width = 500
 canvas_height = 300
 
 def paint( event ):
-   color = "#000000"
-   print("clicked at: ", event.x, event.y)
-   x1, y1 = ( event.x - 0.5), ( event.y -0.55 )
-   x2, y2 = ( event.x + 0.5 ), ( event.y + 0.5 )
-   w.create_oval( x1, y1, x2, y2, fill = color )
+   cor=[]
+   con=1
+   i=0
+   j=1
+   while (con<=2):
+      color = "#000000"
+      print("clicked at: ", event.x, event.y)
+      cor.append(event.x)
+      cor.append( event.y )
+      w.create_oval( cor[i], cor[j], cor[i]-0.5, cor[j]+0.5, fill = color )
+      con += 1
+      i=i+2
+      j=j+2
+      
+   print(cor[0],cor[1],cor[2],cor[3])
+
+   
 
 master = Tk()
-master.title( "hijueperra" )
+master.title( "FIGURAS" )
 w = Canvas(master, 
            width=canvas_width, 
            height=canvas_height)
@@ -21,6 +35,6 @@ w.bind( "<Button-1>", paint )
 message = Label( master, text = "dibuje" )
 message.pack( side = BOTTOM )
     
-mainloop()
 
+window.mainloop()
 
