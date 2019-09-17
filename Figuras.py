0db171868779d382e1d2bf91fa4e415b67faae66
NABA-OL <49452316+NABA-OL@users.noreply.github.com>
2019-09-17 11:44:40 -0500

    Add files via upload

--- Figuras.py
+++ Figuras.py
@@ -1,20 +1,28 @@
 from tkinter import *
 import math
+from pynput.mouse import Listener
 
 
 canvas_width = 500
 canvas_height = 300
 
-def paint( event ):
-   cor=[]
+cor=[]
+master = Tk()
+master.title( "FIGURAS" )
+w = Canvas(master, 
+           width=canvas_width, 
+           height=canvas_height)
+w.pack(expand = YES, fill = BOTH)
+
+def paint( x,y, button, pressed ):
+   
    con=1
    i=0
    j=1
    while (con<=2):
       color = "#000000"
-      print("clicked at: ", event.x, event.y)
-      cor.append(event.x)
-      cor.append( event.y )
+      cor.append(event.x), cor.append( event.y )
+      print(cor[i], cor[j])
       w.create_oval( cor[i], cor[j], cor[i]-0.5, cor[j]+0.5, fill = color )
       con += 1
       i=i+2
@@ -24,17 +32,12 @@ def paint( event ):
 
    
 
-master = Tk()
-master.title( "FIGURAS" )
-w = Canvas(master, 
-           width=canvas_width, 
-           height=canvas_height)
-w.pack(expand = YES, fill = BOTH)
+
 w.bind( "<Button-1>", paint )
 
 message = Label( master, text = "dibuje" )
 message.pack( side = BOTTOM )
     
 
-window.mainloop()
+mainloop()
 
