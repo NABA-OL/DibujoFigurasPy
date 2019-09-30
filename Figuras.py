7c16f47fd63ce8f2a76fb9e115f019782ac98e3c
NABA-OL <NABA-OL@users.noreply.github.com>
2019-09-30 18:21:01 -0500

    Update Figuras.py

--- Figuras.py
+++ Figuras.py
@@ -1,163 +1,114 @@
 import math
 from tkinter import*
+import tkinter as tk
 """NABA-OL"""
 
         
 class Figura:
     global coordenadas
+    global coordenadasy
+    global coordenadasx
     def posicion(event):
-        coordenadas.append(str(event.x)+","+str(event.y))
+        coordenadasx.append(event.x)
+        coordenadasy.append(event.y)
         ventana.create_oval(event.x-1,event.y-1,event.x+1,event.y+1,fill="black",outline="white", width=0)
-    def __init__(self, p1x, p1y, p2x, p2y):
-        self.p1x=int(coordenadas[0])
-        self.p2x=int(coordenadas[1])
-        self.p1y=int(coordenadas[2])
-        self.p2y=int(coordenadas[3])
 
-    def distancia(self, p1x, p1y, p2x=0, p2y=0):
-        math.sqrt((p1x-p2x)**2+(p1y-p2y)**2)
-        return distancia
-
-    def linea():
-        
-        s = -1
-        for pos in coordenadas:
-            xy = pos.split(",")
-            x = xy[0]
-            y =  xy[1]
-            if s== -1:
-                s = 1
-                antx = x
-                anty = y
-            else:
-                ventana.create_line(antx,anty,x,y,smooth=TRUE,fill="black")
-                antx = x
-                anty = y
-
+    def calcArea(resultarea):
+        self.resultarea
+    
+    def calcPerimetro(resultperimetro):
+        self.resultarea
               
-
 class Circulo (Figura):
     global coordenadas
-    def __init__(self, p1x, p1y, p2x, p2y):
-        self.p1x=p1x
-        self.p2x=p2x
-        self.p1y=p1y
-        self.p2y=p2y    
+    global coordenadasy
+    global coordenadasx
+    def distancia():
+        coordenadasx.append(xx[0]-xx[1])
+        coordenadasy.append(yy[0]-yy[1])
+        diagonal=(math.sqrt((pow(coordenadasx[2],2))+(pow(coordenadasy[2],2))))
+        radio=diagonal/2
+    def calcArea(resultarea):
+        resultarea.set("Area:"+str(math.pi*(pow(distancia.radio,2))))
+        return resultarea
     
-    def calcArea(self):
-        self.dist = self.distancia(self.p1x, self.p1y, self.p2x, self.p2y)
-        self.area = math.pi * (self.dist**2)
-        return self.area
-    
-    def calcPerimetro(self):
-        self.dist = self.distancia(self.p1x, self.p1y, self.p2x, self.p2y)
-        self.perimetro = 2 * math.pi * self.dist
-        return self.perimetro
+    def calcPerimetro(resultperimetro):
+        resultperimetro.set("Perimetro:"+str(2*math.pi*distancia.radio))
+        return resultperimetro
 
     def circulo():
         
-        s = -1
-        for pos in coordenadas:
-            xy = pos.split(",")
-            x = xy[0]
-            y =  xy[1]
-            if s== -1:
-                s = 1
-                antx = x
-                anty = y
-            else:
-                ventana.create_oval(int(x),int(y),int(antx),int(anty),fill="skyblue",outline="white",width=0)
-                antx = x*2
-                anty = y*2
+        ventana.create_oval(coordenadasx[0],coordenadasy[0],coordenadasx[1], coordenadasy[1],fill="skyblue")
     
 class Rectangulo(Figura):
     global coordenadas
-    def __init__(self, p1x, p1y, p2x, p2y):
-        self.p1x=p1x
-        self.p2x=p2x
-        self.p1y=p1y
-        self.p2y=p2y
+    global coordenadasy
+    global coordenadasx
     
-    def calcArea1 (self):
-        self.base = self.p2x-self.p1x
-        self.altura = self.p2y-self.p1y  
-        self.area=self.base*self.altura
-        return self.area
+    def calcArea (resultarea):
+        resultarea.set("Area:"+str((coordenadax[0]-coordenadax[1])*(coordenadasy[0]-coordenadasy[1])))
+        return resultarea
+    def calcPerimetro(resultperimetro):
+        resultperimetro.set("Perimetro:"+str((abs(coordenadax[0]-coordenadax[1])*2)+(abs(coordenadasy[0]-coordenadasy[1])*2)))
+        return resultperimetro
     
-    def calcArea (self):
-        return self.calcArea1()
-    
-    def calcPerimetro(self):
-        return (2*self.distancia(self.p1x, self.p2x))+(2*self.distancia(self.p1y, self.p2y))
-
     def rectangulo():
        
-        s = -1
-        for pos in coordenadas:
-            xy = pos.split(",")
-            x = xy[0]
-            y =  xy[1]
-            if s== -1:
-                s = 1
-                antx = x
-                anty = y
-            else:
-                ventana.create_rectangle(antx,anty,x,y,fill="blue",width=0)
-                antx = x
-                anty = y
-    
-class Triangulo (Rectangulo):
+        ventana.create_rectangle(coordenadasx[1], coordenadasy[0],coordenadasx[0], coordenadasy[1],fill="black")
+            
+class Triangulo (Figura):
     global coordenadas
-    def __init__(self, p1x, p1y, p2x, p2y):
-        self.p1x=p1x
-        self.p2x=p2x
-        self.p1y=p1y
-        self.p2y=p2y
+    global coordenadasy
+    global coordenadasx
     
     def calcArea (self):
-        return self.calcArea1()/2
+        resultarea.set("Area:"+str(((coordenadasx[0]-coordenadasx[1])*(coordenadasy[0]-coordenadasy[1]))/2))
+        return resultarea
     
     def calcPerimetro(self):
-        return self.distancia(self.p1x, self.p2x)+self.distancia(self.p1y, self.p2y)+self.distancia(self.p1x, self.p1y, self.p2x, self.p2y)
+        diagonal=(math.sqrt((pow(coordenadasx[2],2))+(pow(coordenadasy[2],2))))
+        resultperimetro.set("Perimetro:"+str(abs(coordenadasx[0]-coordenadasx[1])+abs(coordenadasy[0]-coordenadasy[1])+diagonal))
+        return resultperimetro
 
-    def dtriangulo():
+    def triangulo():
+        ventana.create_line(coordenadasx[0], coordenadasy[0],coordenadasx[1], coordenadasy[1],fill="white")
+        ventana.create_line(coordenadasx[0], coordenadasy[1],coordenadasx[1], coordenadasy[1],fill="white")
+        ventana.create_line(coordenadasx[0], coordenadasy[1],coordenadasx[0], coordenadasy[0],fill="white")
         
-        s = -1
-        for pos in coordenadas:
-            xy = pos.split(",")
-            x = xy[0]
-            y =  xy[1]
-            if s== -1:
-                s = 1
-                antx = x
-                anty = y
-            else:
-                ventana.create_polygon(antx,anty,x,y,int(x)-int(anty),int(y)+int(antx),fill='yellow',outline='white', width=0)
-                antx = x
-                anty = y
+
 
 root = Tk()
-root.geometry("400x430")
+root.geometry("400x480")
 root.title("Figuras")
 coordenadas  = []
+coordenadasx=[]
+coordenadasy=[]
 ventana = Canvas(root,width=400,height=400,background="grey")
 ventana.pack()
 ventana.place(x=0,y=0)
 ventana.bind("<Button-1>", Figura.posicion)
+resultarea= tk.StringVar()
+resultperimetro= tk.StringVar()
 
-btl = Button(root,text="Linea",command=Figura.linea)
 btc = Button(root,text="Circulo",command=Circulo.circulo)
+
 btr = Button(root,text="Rectangulo",command=Rectangulo.rectangulo)
-btt = Button(root,text="Triangulo",command=Triangulo.dtriangulo)
+btt = Button(root,text="Triangulo",command=Triangulo.triangulo)
+btA=Button(root,text="Area",command=Figura.calcArea)
+btA.pack()
+btA.place(x=200,y=403)
 
 btt.pack()
-btt.place(x=172,y=403)
-btl.pack()
-btl.place(x=10,y=403)
+btt.place(x=122,y=403)
 btc.pack()
-btc.place(x=50,y=403)
+btc.place(x=0,y=403)
 btr.pack()
-btr.place(x=100,y=403)
+btr.place(x=50,y=403)
+
+etiqueta1=tk.Label(root, text = "HOLIS AQUI VA EL AREA", textvariable = "HOLIS AQUI VA EL AREA")
+etiqueta1.place(x=0, y=430)
+etiqueta2=tk.Label(root, text = "HOLIS AQUI VA EL Perimetro" , textvariable = "HOLIS AQUI VA EL peri")
+etiqueta2.place(x=0, y=450)
 
 root.mainloop()
         
